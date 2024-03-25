7bb7851eb941f856b61cde7fc678734a62440a80
Furkan Filiz <furkanfiliz>
2024-03-25 23:12:15 +0300

    Bir yerden başlayalım

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="box css"><img src="/css.png" alt="css" /></div>
+        <div className="box js"><img src="/js.png" alt="js" /></div>
+        <div className="box ts"><img src="/ts.png" alt="ts" /></div>
+        <div className="box ff"><img src="/ff.png" alt="ff" /></div>
+      </div>
+    </>
+  )
+}
+
+export default App
